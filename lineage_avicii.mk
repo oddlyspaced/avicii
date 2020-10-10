a2f709432ddf4154da312ee43b21cca60c115fda
CryllicBuster273 <cryllicbuster273@pixelexperience.org>
2020-10-10 13:30:03 +0530

    avicii: Use Coral October FP And Build Description
    Change-Id: I8103546d91ce237811ba4117c06353481999b9e8

--- lineage_avicii.mk
+++ lineage_avicii.mk
@@ -43,6 +43,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_DEVICE=OnePlusNord \
     PRODUCT_NAME=OnePlusNord \
-    PRIVATE_BUILD_DISC="Nord_IND-user 10 QKQ1.200412.002 2007312126 release-keys"
+    PRIVATE_BUILD_DISC="coral-user 11 RP1A.201005.004 6782484 release-keys"
 
-BUILD_FINGERPRINT := "google/coral/coral:10/QQ3A.200805.001/6578210:user/release-keys"
+BUILD_FINGERPRINT := "google/coral/coral:11/RP1A.201005.004/6782484:user/release-keys"
