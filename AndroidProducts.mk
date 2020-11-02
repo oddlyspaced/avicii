zhuofan-16/device_oneplus_avicii

avicii: call product in the aosp way
Co-authored-by: iamsaalim <saalimquadri1@gmail.com>
Signed-off-by: CryllicBuster273 <cryllicbuster273@pixelexperience.org>

AndroidProducts.mk
@@ -1,0 +1,23 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_avicii-user \
+    aosp_avicii-userdebug \
+    aosp_avicii-eng

oddlyspaced/avicii

AndroidProducts.mk
@@ -1,0 +1,22 @@
+#
+# Copyright (C) 2018-2019 The LineageOS Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/lineage_avicii.mk
+
+COMMON_LUNCH_CHOICES := \
+    lineage_avicii-userdebug \
+    lineage_avicii-eng