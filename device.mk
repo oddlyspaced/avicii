db2f4005cf624b43652b0c80dbdba66c4e5d9ecc
oddlyspaced <oddlyspaced1@gmail.com>
2020-12-28 00:04:24 +0530

    avicii: Disable Google Camera import

--- device.mk
+++ device.mk
@@ -24,7 +24,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
 $(call inherit-product, vendor/oneplus/avicii/avicii-vendor.mk)
 
 # GoogleCamera
-$(call inherit-product, packages/apps/GoogleCamera/config.mk)
+# $(call inherit-product, packages/apps/GoogleCamera/config.mk)
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
